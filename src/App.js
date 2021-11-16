3deb2e77a311df49ccf8d943e5b01d8e3dc2609f
m0hit04 <msrathore0481@gmail.com>
2021-11-16 19:53:50 +0530

    Added styling on name tab

--- src/App.js
+++ src/App.js
@@ -40,7 +40,15 @@ function App() {
 
         <div className="btn-container">
           {jobs.map((item, index) => {
-            return <button key={item.id} onClick={()=>setValue(index)}>{item.company}</button>;
+            return (
+              <button
+              key={item.id}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index===value && 'active-btn'}`}
+                >
+                {item.company}
+              </button>
+            );
           })}
         </div>
 
